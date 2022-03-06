b3c83072bd3265324bbcdb10b901be5289ed7411
Gia Kinh <giakinh2000@gmail.com>
2022-03-06 16:29:50 +0700

    add error page

---
+++ src/java/controller/error/ErrorHandler.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.error;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author giaki
+ */
+public class ErrorHandler extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        processError(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        processError(request, response);
+    }
+
+    private void processError(HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
+        //customize error message
+        Throwable throwable = (Throwable) request
+                .getAttribute("javax.servlet.error.exception");
+        Integer statusCode = (Integer) request
+                .getAttribute("javax.servlet.error.status_code");
+        String servletName = (String) request
+                .getAttribute("javax.servlet.error.servlet_name");
+        if (servletName == null) {
+            servletName = "Unknown";
+        }
+        String requestUri = (String) request
+                .getAttribute("javax.servlet.error.request_uri");
+        if (requestUri == null) {
+            requestUri = "Unknown";
+        }
+        request.setAttribute("error", "Servlet " + servletName
+                + " has thrown an exception " + throwable.getClass().getName()
+                + " : " + throwable.getMessage());
+        request.getRequestDispatcher("/views/error/error.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
