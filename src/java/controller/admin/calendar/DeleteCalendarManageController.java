f9f6e3edfd79aa29c2ee1138acda96ad56f3fa67
Gia Kinh <giakinh2000@gmail.com>
2022-03-06 16:51:11 +0700

    fix bug delete calendar

--- src/java/controller/admin/calendar/DeleteCalendarManageController.java
+++ src/java/controller/admin/calendar/DeleteCalendarManageController.java
@@ -7,9 +7,12 @@ package controller.admin.calendar;
 
 import controller.admin.auth.BaseAuthAdminController;
 import dal.auth.UserDBContext;
+import dal.calendar.AdditionalCalendarDBContext;
 import dal.calendar.CalendarDBContext;
+import dal.calendar.EventCalendarDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -18,7 +21,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.auth.User;
+import model.calendar.AdditionalCalendar;
 import model.calendar.Calendar;
+import model.calendar.EventCalendar;
 import utils.Validate;
 
 /**
@@ -53,6 +58,14 @@ public class DeleteCalendarManageController extends BaseAuthAdminController {
             String idString = validate.getField(request, "id", true);
             int id = validate.fieldInt(idString, "Error get field id");
             CalendarDBContext calendarDB = new CalendarDBContext();
+            EventCalendarDBContext eventDB = new EventCalendarDBContext();
+            AdditionalCalendarDBContext additionalDB = new AdditionalCalendarDBContext();
+            ArrayList<AdditionalCalendar> additionals = additionalDB.findMany("calendarId", id+"");
+            for (AdditionalCalendar additional : additionals) {
+                EventCalendar event = eventDB.findOne("additionalId",additional.getId()+"");
+                eventDB.delete(event.getId());
+                additionalDB.delete(additional.getId());
+            }
             calendarDB.delete(id);
             response.sendRedirect(request.getHeader("referer"));
         } catch (Exception ex) {
